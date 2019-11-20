f2fd0d3be12900ca0a4601a0350074eee3bbdc25
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-11-20 10:54:36 +0100

    create initial sample test

--- server/tests/testEndpoints.nim
+++ server/tests/testEndpoints.nim
@@ -2,6 +2,11 @@
 
 import unittest
 import "../src/routes/api.nim"
+import math
 
-test "/api/dp works as expected":
-  check add(5,5) == 10
+test "/api/dp":
+  check sum([5,5]) == 10
+
+test "/api/sp":
+  check sum([5,5]) == 10
+  
