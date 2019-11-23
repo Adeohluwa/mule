f58a1e21697928c4d260da3eb29dab9477b7f880
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-11-23 13:22:49 +0100

    encapsulate all controllers

---
+++ server/src/routes/controllers.nim
@@ -0,0 +1,20 @@
+import createUser 
+
+import onlineUsers
+
+import deliverPackage
+import sendPackage
+
+import getReview
+import createReview
+
+import createBooking
+import cancelBooking
+
+import phoneContact
+
+import trackDelivery
+import confirmDelivery
+
+
+export controllers
