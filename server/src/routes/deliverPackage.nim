24207ab2db6ce5d94fd7ac186750928f75939d82
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2019-11-23 12:51:42 +0100

    add controller to deliver package

---
+++ server/src/routes/deliverPackage.nim
@@ -0,0 +1,9 @@
+proc deliverPackage*(): JsonNode =
+   # var mule  = UserSchema(
+    #  name: request.body[user],
+    #  origin: request.body[origin],
+    #  destination: request.body[destination],
+    #  online: true
+    #)
+    #mule.insert()
+    #%*mule
